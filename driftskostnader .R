f6d201aee520d55087e5e19785091ca16439c216
Izolda Vlasova <izoldavlasova65111@gmail.com>
2022-07-15 18:29:25 +0200

    Data on operating costs from SSB (2010-2021)

---
+++ driftskostnader .R
@@ -0,0 +1,86 @@
+options(encoding="UTF-8")
+library(httr)
+
+
+
+# henter rjstat bibliotek for behandling av JSON-stat
+library(rjstat)
+url <- "https://data.ssb.no/api/v0/no/table/06464/"
+
+
+
+# spørring fra konsoll - kan være på en linje
+data <- '
+{
+  "query": [
+    {
+      "code": "HelseReg",
+      "selection": {
+        "filter": "vs:Helsereg3",
+        "values": [
+          "H12",
+          "H03",
+          "H04",
+          "H05"
+        ]
+      }
+    },
+    {
+      "code": "HelseRegnKost",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "000"
+        ]
+      }
+    },
+    {
+      "code": "ContentsCode",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "Per10000"
+        ]
+      }
+    },
+    {
+      "code": "Tid",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "2010",
+          "2011",
+          "2012",
+          "2013",
+          "2014",
+          "2015",
+          "2016",
+          "2017",
+          "2018",
+          "2019",
+          "2020",
+          "2021"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
+'
+d.tmp <- POST(url , body = data, encode = "json", verbose())
+
+
+
+# Henter ut innholdet fra d.tmp som tekst deretter bearbeides av fromJSONstat
+driftskostander <- fromJSONstat(content(d.tmp, "text"))
+
+
+
+# Viser datasettet
+driftskostander 
+
+# Clean the column names 
+driftskostander <- driftskostander %>% 
+  clean_names()
