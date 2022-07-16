23b84a0ddf5b32e10f97dd8d83a9f5cd71ae6eb3
Izolda Vlasova <izoldavlasova65111@gmail.com>
2022-07-16 16:45:14 +0200

    Visualising with plotly

--- merged_data_plot .R
+++ merged_data_plot .R
@@ -5,6 +5,7 @@ library(janitor)
 library(tidyverse)
 library(rjstat)
 library(tidyr)
+library(plotly)
 
 # henter rjstat bibliotek for behandling av JSON-stat
 url <- "https://data.ssb.no/api/v0/no/table/06464/"
@@ -297,6 +298,8 @@ dognplasser <- dognplasser %>%
 driftskostander <- driftskostander %>% 
   select(region, ar, value)
 
+
+
 # Population per health region 
 sor_ost_pop <- 3.1 
 nord_pop <- 0.5 
@@ -334,6 +337,29 @@ merged_data <- merged_data %>%
 merged_data <- merged_data %>% 
   rename(dagbehandlinger = dagbehandling_oppholdsdager_per_1_000_innbyggere)
 
+# Deviding by 1000 because the initial data is measured per 1000 people 
+merged_data <- merged_data %>% 
+  mutate(konsultasjoner = konsultasjoner/1000)
+merged_data <- merged_data %>% 
+  mutate(value_arsverk = value_arsverk/1000000)
+merged_data <- merged_data %>% 
+  mutate(oppholdsdogn = oppholdsdogn/1000)
+merged_data <- merged_data %>% 
+  mutate(dagbehandlinger = dagbehandlinger/1000)
+merged_data <- merged_data %>% 
+
+merged_data <- merged_data %>% 
+  mutate(value_dognplasser = ifelse(region == "Helseregion Sør-Øst (2007-)", value_dognplasser/sor_ost_pop, value_dognplasser))
+
+merged_data <- merged_data %>% 
+  mutate(value_dognplasser = ifelse(region == "Helseregion Vest", value_dognplasser/vest_pop, value_dognplasser))
+
+merged_data <- merged_data %>% 
+  mutate(value_dognplasser = ifelse(region == "Helseregion Midt-Norge", value_dognplasser/midt_pop, value_dognplasser))
+
+merged_data <- merged_data %>% 
+  mutate(value_dognplasser = ifelse(region == "Helseregion Nord", value_dognplasser/nord_pop, value_dognplasser))
+
 # Man years vs consultations 
 ggplot(merged_data, aes(x = value_arsverk, y = konsultasjoner, group = region)) + 
   geom_point() + 
@@ -349,6 +375,15 @@ ggplot(merged_data, aes(x = value_arsverk, y = oppholdsdogn, group = region)) +
   geom_point() + 
   facet_grid(vars(region))
 
+#Doing the same with plotly (without facet_grid) 
+merged_data %>% 
+  plot_ly(x = ~value_arsverk, y = ~oppholdsdogn, type = "scatter", color = ~region)
+
+#Here you can see that Helse Nord produces around the same amounts of consultations 
+# while using more man-years per one person 
+merged_data %>% 
+  plot_ly(x = ~value_arsverk, y = ~konsultasjoner, type = "scatter", color = ~region, text = ~ar)
+
 # Helse Sør-Øst has the highest population - around 3.1 million people (source: https://helse-sorost.no/om-oss/vart-oppdrag/hva-har-vi-gjort/nokkeltall)
 # Helse Nord has population of around 0.5 million people (source: https://helse-nord.no/om-oss/hva-gjor-helse-nord-rhf/nokkeltall)
 # Helse Vest has population of around 1.1 million people (source: https://helse-vest.no/om-oss/nokkeltal)
@@ -358,11 +393,34 @@ ggplot(merged_data, aes(x = value_arsverk, y = oppholdsdogn, group = region)) +
 # Interactive visualisations with plotly 
 # merge data from Helsedirektoratet 
 
+# Filtering for year 2020 
+merged_data_2020 <- merged_data %>% 
+  filter(ar == 2020)
 
+# Number of 24 hours stays vs number of man-years (not really related)
+merged_data_2020 %>% 
+  plot_ly(x = ~value_arsverk, y = ~oppholdsdogn, type = "scatter", color = ~region)
 
+# Number of consultations vs number of man-years 
+merged_data_2020 %>% 
+  plot_ly(x = ~value_arsverk, y = ~konsultasjoner, type = "scatter", color = ~region)
 
+# Number of 24 hour stays vs beds 
+merged_data_2020 %>% 
+  plot_ly(x = ~value_dognplasser, y = ~oppholdsdogn, type = "scatter", color = ~region)
 
+# Filtering for health regions 
+sor_ost_data <- merged_data %>% 
+  filter(region == "Helseregion Sør-Øst (2007-)")
+midt_data <- merged_data %>% 
+  filter(region == "Helseregion Midt-Norge")
 
+# Visualising health regions data 
+sor_ost_data %>% 
+  plot_ly(x = ~value_arsverk, y = ~konsultasjoner, type = "scatter", color = ~region, text = ~ar)
+
+midt_data %>% 
+  plot_ly(x = ~value_arsverk, y = ~konsultasjoner, type = "scatter", color = ~region, text = ~ar)
 
 
 
